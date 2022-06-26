3f343c7481f46cb8ccdfcaa707c0a55f1cd11a1e
임보라 <bora.im@muzlive.com>
2022-06-26 17:31:40 +0900

    local

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Room
+from .serializers import ReadRoomSerializser, WriteRoomSerializser
 
-# Create your views here.
+
+@api_view(["GET", "POST"])
+def rooms_view(request):
+    if request.method == "GET":
+        rooms = Room.objects.all()
+        serializer = ReadRoomSerializser(rooms, many=True).data
+        return Response(serializer)
+    elif request.method == "POST":
+        if not request.user.is_authenticated:
+            return Response(status.HTTP_401_UNAUTHORIZED)
+        serializer = WriteRoomSerializser(data=request.data)
+        if serializer.is_valid():
+            room = serializer.save(user=request.user)
+            room_serializer = ReadRoomSerializser(room).data
+            return Response(data=room_serializer, status=status.HTTP_200_OK)
+        else:
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RoomsView(APIView):
+    def get(self, request):
+        rooms = Room.objects.all()
+        serializer = ReadRoomSerializser(rooms, many=True).data
+        return Response(serializer)
+
+    def post(self, request):
+        if not request.user.is_authenticated:
+            return Response(status.HTTP_401_UNAUTHORIZED)
+        serializer = WriteRoomSerializser(data=request.data)
+        if serializer.is_valid():
+            room = serializer.save(user=request.user)
+            room_serializer = ReadRoomSerializser(room).data
+            return Response(data=room_serializer, status=status.HTTP_200_OK)
+        else:
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RoomView(APIView):
+    def get(self, request):
+        pass
+
+    def put(self, request):
+        pass
+
+    def delete(self, request):
+        pass
